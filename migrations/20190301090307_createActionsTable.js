fe90469dad05585778f73e64ac23108794344fe8
J A <jarata@gmail.com>
2019-03-01 09:18:56 -0800

    projects and actions migrations

---
+++ migrations/20190301090307_createActionsTable.js
@@ -0,0 +1,14 @@
+
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('actions', function(tbl) {
+        tbl.increments();
+        tbl.string('actionDescription', 128)
+            .notNullable();
+        tbl.string('notes', 128);
+        tbl.boolean('completed')
+    })
+};
+
+exports.down = function(knex, Promise) {
+  
+};
